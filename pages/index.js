17405df6a1d054b5056717a0d860971f904fbf6f
Devian Reza <99148003+devianre@users.noreply.github.com>
2022-02-07 05:54:38 +0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -6,8 +6,8 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Next.js Starter!</title>
-        <link rel="icon" href="/favicon.ico" />
+        <title>Ploon24.link!</title>
+        <link rel="text" href="https://www.ploon24.link" />
       </Head>
 
       <main>
